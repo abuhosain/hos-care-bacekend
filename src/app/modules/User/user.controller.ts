eb23530a2748f58c26ce96ccfee91b27c63e6b83
Hosain <cryptichasan925@gmail.com>
2024-11-04 22:17:40 +0600

    response formating

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -3,8 +3,20 @@ import { UserService } from "./user.services";
 
 const createAdmin = async (req: Request, res: Response) => {
   // console.log(req.body);
-  const result = await UserService.createAdmin(req.body);
-  res.send(result);
+  try {
+    const result = await UserService.createAdmin(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin created successfully",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something went wron",
+      error: err,
+    });
+  }
 };
 
 export const UserController = {
