40254687cbd890e8f7fe79719004648858fcbe10
Hosain <cryptichasan925@gmail.com>
2024-11-05 00:18:52 +0600

    advance searching

--- src/app/modules/Admin/admin.services.ts
+++ src/app/modules/Admin/admin.services.ts
@@ -3,14 +3,27 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prsima = new PrismaClient();
 
 const getAllAdmin = async (params: any) => {
+  const { searchTerm, ...filterData } = params;
   const andConditions: Prisma.AdminWhereInput[] = [];
   const adminSearchAbleFields = ["name", "email"];
+  console.log(filterData);
+
   if (params.searchTerm) {
     andConditions.push({
       OR: adminSearchAbleFields.map((field) => ({
         [field]: {
           contains: params.searchTerm,
           mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData.length > 0)) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
         },
       })),
     });
