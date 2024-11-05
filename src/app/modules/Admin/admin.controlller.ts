e362d43db1c3fca1e5defeebb60a9b755667b352
Hosain <cryptichasan925@gmail.com>
2024-11-05 23:47:39 +0600

    pagination done

--- src/app/modules/Admin/admin.controlller.ts
+++ src/app/modules/Admin/admin.controlller.ts
@@ -1,28 +1,13 @@
 import { Request, Response } from "express";
 import { AdminServices } from "./admin.services";
-
-const pick = <T extends Record<string, unknown>, K extends keyof T>(
-  obj: T,
-  keys: K[]
-): Partial<T> => {
-  const finalObj: Partial<T> = {};
-  for (const key of keys) {
-    if (obj && Object.hasOwnProperty.call(obj, key)) {
-      finalObj[key] = obj[key];
-    }
-  }
-  return finalObj;
-};
+import pick from "../../../shared/pick";
+import { adminFilterableFields } from "./admin.contstance";
 
 const getAllAdmin = async (req: Request, res: Response) => {
   try {
-    const filters = pick(req.query, [
-      "name",
-      "email",
-      "searchTerm",
-      "contactNumber",
-    ]);
-    const result = await AdminServices.getAllAdmin(filters);
+    const filters = pick(req.query, adminFilterableFields);
+    const options = pick(req.query, ["limit", "page"]);
+    const result = await AdminServices.getAllAdmin(filters, options);
     res.status(200).json({
       success: true,
       message: "Admin fetched successfully",
--- src/app/modules/Admin/admin.services.ts
+++ src/app/modules/Admin/admin.services.ts
@@ -1,11 +1,12 @@
 import { Prisma, PrismaClient } from "@prisma/client";
+import { adminSearchAbleFields } from "./admin.contstance";
 
 const prsima = new PrismaClient();
 
-const getAllAdmin = async (params: any) => {
+const getAllAdmin = async (params: any, options: any) => {
+  const { limit, page } = options;
   const { searchTerm, ...filterData } = params;
   const andConditions: Prisma.AdminWhereInput[] = [];
-  const adminSearchAbleFields = ["name", "email"];
 
   if (params.searchTerm) {
     andConditions.push({
@@ -32,6 +33,8 @@ const getAllAdmin = async (params: any) => {
 
   const result = await prsima.admin.findMany({
     where: whereConditions,
+    skip: (Number(page) - 1) * Number(limit),
+    take: Number(limit),
   });
   return result;
 };
