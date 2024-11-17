7112a5345f5615d3cf6cdcb59c9d5343ef47a008
Hosain <cryptichasan925@gmail.com>
2024-11-17 22:01:32 +0600

    api Errro implement

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { JwtHelpers } from "../helpers/jwtHelpers";
+import { Secret } from "jsonwebtoken";
+import config from "../config";
+import ApiError from "../errors/ApiError";
+import httpStatus from "http-status";
+
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized");
+      }
+      const verifiedUser = JwtHelpers.verifyToken(
+        token,
+        config.jwt.jwt_secret as Secret
+      );
+      //   console.log(verifiedUser);
+      if (roles.length && !roles.includes(verifiedUser.role)) {
+        throw new ApiError(httpStatus.FORBIDDEN, "You are not authorized");
+      }
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+export default auth;
