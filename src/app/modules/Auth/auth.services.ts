4bd6557137deaec45afbeb0246b862aaebfc0fd3
Hosain <cryptichasan925@gmail.com>
2024-11-17 12:15:37 +0600

    env setup done

--- src/app/modules/Auth/auth.services.ts
+++ src/app/modules/Auth/auth.services.ts
@@ -1,8 +1,9 @@
 import { prisma } from "../../../shared/prisma";
 import * as bcrypt from "bcrypt";
 import { JwtHelpers } from "../../helpers/jwtHelpers";
-import jwt, { JwtPayload } from "jsonwebtoken";
 import { UserStatus } from "@prisma/client";
+import config from "../../config";
+import { Secret } from "jsonwebtoken";
 
 const loginUser = async (payload: { email: string; password: string }) => {
   const userData = await prisma.user.findUniqueOrThrow({
@@ -23,14 +24,14 @@ const loginUser = async (payload: { email: string; password: string }) => {
 
   const accessToken = JwtHelpers.generateToken(
     { email: userData.email, role: userData.role },
-    "abcdefg",
-    "5m"
+    config.jwt.jwt_secret as Secret,
+    config.jwt.expires_in as string
   );
 
   const refreshToken = JwtHelpers.generateToken(
     { email: userData.email, role: userData.role },
-    "abcdefgh",
-    "30d"
+    config.jwt.refresh_token as Secret,
+    config.jwt.refresh_token_expires_in as string
   );
 
   return {
